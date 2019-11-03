b7b3fbdcff3d5678d487d2e6a0e53220e9bc442a
kartikeybhardwaj25 <38663572+kartikeybhardwaj25@users.noreply.github.com>
2019-11-03 19:43:27 +0530

    committed

--- Webhandles_Class1_October2019/src/edu/Alert/AlertPopUp.java
+++ Webhandles_Class1_October2019/src/edu/Alert/AlertPopUp.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AlertPopUp {
 
 	public static void main(String[] args) {
+		//
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium_Jars\\ChromeDriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
